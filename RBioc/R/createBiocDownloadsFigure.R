e479c7cf4f38ece74f7b8685ebb2f6a37b1540d8
Sebastian Gibb <mail@sebastiangibb.de>
2014-03-31 17:45:05 +0100

    use only unique ips for couting the downloads

--- RBioc/R/createBiocDownloadsFigure.R
+++ RBioc/R/createBiocDownloadsFigure.R
@@ -10,6 +10,9 @@ colnames(df) <- c("pkgversion", "biocversion", "utc_offset", "protocol",
                   "biocrepo", "day_month_year", "url", "package", "bytes",
                   "errorcode", "ips", "pkgtype", "referer", "user_agent", "time",
                   "month_year", "method", "biocrepo_relurl")
+## remove duplicates
+df <- df[!duplicated(df[, c("package", "ips", "month_year")]), ]
+
 ## reduce df
 df <- df[, c("package", "pkgversion", "biocversion", "day_month_year", "biocrepo_relurl")]
 
@@ -49,7 +52,10 @@ createBiocViewDownloadFigure <- function(downloadDf, views, biocVersions,
 
   ## 2.12 has some really strange download counts (more than 10fold above the
   ## 2.13). We interpolate the download number of 2.12 using the mean of 2.11
-  ## and 2.13
+  ## and 2.13.
+  ## But we noticed (from bioc download stats for mzR and xcms) that parts of
+  ## 2.12 (Aug/Sep/Oct 2013) and 2.13 (Oct/Nov/Dec 2013, Jan/Feb 2014) are also
+  ## affected.
   bc212Idx <- match("2.12", biocVersions, nomatch=0)
 
   if (bc212Idx > 1 && bc212Idx < length(biocVersions)) {
